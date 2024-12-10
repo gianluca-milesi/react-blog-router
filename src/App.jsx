d27d0086dc7e9840984b7289882b9f6a70510a77
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-12-10 21:00:47 +0100

    ADD: Blank layout route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 //Pages
+import NotFound from "./pages/NotFound/NotFound.jsx"
 import Home from "./pages/Home/Home.jsx"
 import About from "./pages/About/About.jsx"
 import PostsIndex from "./pages/posts/PostsIndex/PostsIndex.jsx"
 import PostsShow from "./pages/posts/PostsShow/PostsShow.jsx"
 //Layouts
 import DefaultLayout from "./layouts/DefaultLayout/DefaultLayout.jsx"
+import BlankLayout from "./layouts/BlankLayout/BlankLayout.jsx"
 
 
 function App() {
@@ -21,6 +23,9 @@ function App() {
               <Route path="" element={<PostsIndex />}></Route>
               <Route path=":id" element={<PostsShow />}></Route>
             </Route>
+          </Route>
+          <Route element={<BlankLayout />}>
+            <Route path="*" element={<NotFound />}></Route>
           </Route>
         </Routes>
       </BrowserRouter >
