ecdf3dd6f011690be46e454634b1030880d98d2a
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-12-10 18:00:47 +0100

    ADD: fetch api on PostsShow component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,6 @@
-import { API_BASE_URI } from "../../config.js"
 import style from "./Card.module.css"
+import { API_BASE_URI } from "../../config.js"
+import { Link } from "react-router-dom"
 
 
 function Card({ item = {}, deleteItem = () => { } }) {
@@ -7,14 +8,16 @@ function Card({ item = {}, deleteItem = () => { } }) {
     const { id, title, image, content } = item
 
     return (
-        <div className={style.card}>
-            <img src={`${API_BASE_URI}${image}`} className={style.card_figure} />
-            <div className={style.card_body}>
-                <h3>{title}</h3>
-                <p>{content}</p>
+        <Link to={`/posts/${id}`}>
+            <div className={style.card}>
+                <img src={`${API_BASE_URI}${image}`} className={style.card_figure} />
+                <div className={style.card_body}>
+                    <h3>{title}</h3>
+                    <p>{content}</p>
+                </div>
+                <button className={style.delete_button} onClick={() => deleteItem(id)}>Elimina</button>
             </div>
-            <button className={style.delete_button} onClick={() => deleteItem(id)}>Elimina</button>
-        </div>
+        </Link>
     )
 }
 
--- src/pages/posts/PostsShow/PostsShow.jsx
+++ src/pages/posts/PostsShow/PostsShow.jsx
@@ -1,11 +1,55 @@
 import style from "./PostsShow.module.css"
 import sectionsStyle from "../../Sections.module.css"
+import placeholder from "../../../assets/placeholder.png"
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom";
+import { API_BASE_URI } from "../../../config"
+
 
 function PostsShow() {
+
+    const { id } = useParams()
+    const [post, setPost] = useState({})
+    const { title, image, content, tags = [], category } = post
+
+    function fetchPost() {
+        axios.get(`${API_BASE_URI}posts/${id}`)
+            .then((res) => {
+                setPost(res.data)
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    }
+
+    useEffect(() => {
+        fetchPost()
+    }, [id])
+
+
     return (
-        <section>
-            <div>SHOW PAGE</div>
-        </section>
+        <main>
+            <section className="hero">
+                <div className="container">
+                    <h1>{title}</h1>
+                    <figure>
+                        <button className="button_back">Indietro</button>
+                        <img src="..." />
+                    </figure>
+                </div>
+            </section>
+
+            <section className="information">
+                <div className="container">
+                    <div className="information_body">
+                        <p>{content}</p>
+                        <p>{tags}</p>
+                        <p>{category}</p>
+                    </div>
+                </div>
+            </section>
+        </main>
     )
 }
 
